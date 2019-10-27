d3a918814c9f03dd5c2a3f34113151c7cbd4fe8e
Saulo <sauloacalixto@gmail.com>
2019-10-27 08:40:57 -0300

    Implementando validações de questão
    Foi feito um levantamento do requisito de questões e com base neles foi implementado validações e testes para esses cenários.
    
    TODO
    Validações que exigiram ir ao banco não foram testadas, pois houve um problema para mockar o repositório, isso será corrigido com mais tempo.

--- listaelab-testes/TesteBase.cs
+++ listaelab-testes/TesteBase.cs
@@ -6,14 +6,21 @@ namespace ListElab.Testes
 {
     public abstract class TesteBase<T> where T : ObjetoComId
     {
-        public void EfetueChecagem(bool ehParaDarErro, T objetoValidado, ValidadorPadrao<T> validador, string mensagem)
+        /// <summary>
+        /// Verifica se a validação foi acionada ou não.
+        /// </summary>
+        /// <param name="cenarioInvalido">True caso o cenário testado retorne uma inconsistência, False, caso não.</param>
+        /// <param name="objetoValidado">O objeto a ser validado.</param>
+        /// <param name="validador">O validador do cenário.</param>
+        /// <param name="mensagemDeValidacao">A mensagem a ser retornada caso o cenário seja inválido.</param>
+        public void ValideTeste(bool cenarioInvalido, T objetoValidado, ValidadorPadrao<T> validador, string mensagemDeValidacao)
         {
             var resultado = validador.Validate(objetoValidado);
 
-            if (ehParaDarErro)
+            if (cenarioInvalido)
             {
                 Assert.IsFalse(resultado.IsValid);
-                Assert.AreEqual(resultado.Errors[0].ErrorMessage, mensagem);
+                Assert.AreEqual(resultado.Errors[0].ErrorMessage, mensagemDeValidacao);
             }
             else
             {
--- listelab-servico/Validacoes/ValidacoesQuestaoDiscursiva.cs
+++ listelab-servico/Validacoes/ValidacoesQuestaoDiscursiva.cs
@@ -1,11 +1,17 @@
 ﻿using FluentValidation;
+using ListElab.Data.Repositorios;
 using ListElab.Dominio.Conceitos.QuestaoObj;
 using ListElab.Dominio.Conceitos.RespostaObj;
+using ListElab.Dominio.Conceitos.UsuarioObj;
+using ListElab.Dominio.Enumeradores;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace ListElab.Servico.Validacoes
 {
+    /// <summary>
+    /// Validador de questões discursivas.
+    /// </summary>
     public class ValidacoesQuestaoDiscursiva : ValidadorPadrao<Questao<Discursiva>>
     {
         /// <summary>
@@ -21,20 +27,79 @@ namespace ListElab.Servico.Validacoes
         /// <summary>
         /// Número do requisito.
         /// </summary>
-        public void AssineRegraPalavraChaveInformado()
+        public void AssineRegraDificuldadeFoiInformadaEValida()
+        {
+            RuleFor(questao => questao.NivelDificuldade)
+                .Must(dificuldade => (int)dificuldade >= 1 && (int)dificuldade <= 5)
+                .WithMessage("Informe um valor válido para nível de dificuldade");
+        }
+
+        /// <summary>
+        /// Número do requisito.
+        /// </summary>
+        public void AssineRegraTipoQuestaoDiscursiva()
+        {
+            RuleFor(questao => questao.Tipo)
+                .Must(tipo => tipo == TipoQuestao.Discursiva)
+                .WithMessage("O tipo de questão deve ser 'Discursiva'");
+        }
+
+        /// <summary>
+        /// Número do requisito.
+        /// </summary>
+        public void AssineRegraPalavraChaveInformada()
         {
             RuleFor(questao => questao.RespostaEsperada.PalavrasChaves)
                 .Must(ValidePalavrasChaves)
                 .WithMessage("Pelo menos uma palavra chave deve ser informada");
         }
 
+        #region VALIDAÇÕES USUARIO
+
+        /// <summary>
+        /// Valida regra que o autor da quesetão deve ser informado e válido.
+        /// </summary>
+        public void AssineRegraAutorDaQuestaoInformadoEValido()
+        {
+            RuleFor(questao => questao.Usuario)
+                .Must(usuario => !string.IsNullOrEmpty(usuario) && usuario.EndsWith("@ufg.br"))
+                .WithMessage("O autor da questão deve ser um usuário válido");
+        }
+
+        /// <summary>
+        /// Valida regra que o autor da questão precisa estar cadastrado no sistema.
+        /// </summary>
+        public void AssineRegraAutorDaQuestaoExiste()
+        {
+            RuleFor(questao => questao.Usuario)
+                .Must(usuario => new Repositorio<Usuario>().ItemExiste(x => x.Email == usuario))
+                .WithMessage("O autor da questão não é um usuário válido");
+        }
+
+        /// <summary>
+        /// Valida regra que o autor da questão não pode ser atualizado.
+        /// </summary>
+        public void AssineRegraAutorDaQuestaoNaoPodeSerAtualizado()
+        {
+            RuleFor(questao => questao.Usuario)
+                .Must(AutorDaQuestaoNaoFoiAtualizado)
+                .When(x => objetoPersistido != null)
+                .WithMessage("O autor da questão não pode ser atualizado");
+        }
+
+        #endregion
+
         /// <summary>
         /// Assina regras para o cenário de cadastro.
         /// </summary>
         protected override void AssineRegrasDeCadastro()
         {
-            AssineRegraPalavraChaveInformado();
+            AssineRegraPalavraChaveInformada();
             AssineRegraDeveTerEnunciado();
+            AssineRegraAutorDaQuestaoInformadoEValido();
+            AssineRegraAutorDaQuestaoExiste();
+            AssineRegraTipoQuestaoDiscursiva();
+            AssineRegraDificuldadeFoiInformadaEValida();
         }
 
         /// <summary>
@@ -42,8 +107,13 @@ namespace ListElab.Servico.Validacoes
         /// </summary>
         protected override void AssineRegrasDeAtualizacao()
         {
-            AssineRegraPalavraChaveInformado();
+            AssineRegraPalavraChaveInformada();
             AssineRegraDeveTerEnunciado();
+            AssineRegraAutorDaQuestaoInformadoEValido();
+            AssineRegraAutorDaQuestaoExiste();
+            AssineRegraAutorDaQuestaoNaoPodeSerAtualizado();
+            AssineRegraTipoQuestaoDiscursiva();
+            AssineRegraDificuldadeFoiInformadaEValida();
         }
 
         /// <summary>
@@ -62,7 +132,12 @@ namespace ListElab.Servico.Validacoes
 
             var listaValidada = palavras;
 
-            return listaValidada.All(x => x != null);
+            return listaValidada.All(x => x != null) && listaValidada.Any(x => !string.IsNullOrEmpty(x.Descricao));
+        }
+
+        private bool AutorDaQuestaoNaoFoiAtualizado(string usuario)
+        {
+            return objetoPersistido.Usuario == usuario;
         }
     }
 }
