8f3b292f456a777d5a4a28806a060a1ac284c482
gustavo-b <gustavo.hsbatista@gmail.com>
2019-12-04 02:02:23 -0300

    Acrescentando validações de questões objetivas e testes

--- listelab-servico/Validacoes/ValidacoesQuestaoAssociacaoDeColuna.cs
+++ listelab-servico/Validacoes/ValidacoesQuestaoAssociacaoDeColuna.cs
@@ -5,7 +5,7 @@ using ListElab.Dominio.Enumeradores;
 
 namespace ListElab.Servico.Validacoes
 {
-    public class ValidacoesQuestaoAssociacaoDeColuna : ValidadorPadrao<Questao<AssociacaoDeColunas>>
+    public class ValidacoesQuestaoAssociacaoDeColuna : ValidacoesQuestao<AssociacaoDeColunas>
     {
         /// <summary>
         /// Tipo de questão deve ser múltipla escolha.
@@ -17,14 +17,70 @@ namespace ListElab.Servico.Validacoes
                 .WithMessage("O tipo de questão deve ser 'Associação de colunas'");
         }
 
+        /// <summary>
+        /// Coluna principal da resposta esperada possui item.
+        /// </summary>
+        public void AssineRegraColunaPrincipalPossuiItem()
+        {
+            RuleFor(questao => questao.RespostaEsperada.Colunas)
+                    .Must(coluna => coluna != null && !coluna.Exists(item => string.IsNullOrEmpty(item.ColunaPrincipal.Letra)))
+                    .WithMessage("A coluna principal deve possuir pelo menos algum item.");
+        }
+
+        /// <summary>
+        /// Coluna associada da resposta esperada possui item.
+        /// </summary>
+        public void AssineRegraColunaAssociadaPossuiItem()
+        {
+            RuleFor(questao => questao.RespostaEsperada.Colunas)
+                        .Must(coluna => coluna != null && !coluna.Exists(item => string.IsNullOrEmpty(item.ColunaAssociada.Letra)))
+                        .WithMessage("A coluna associada deve possuir pelo menos algum item.");
+        }
+
+        /// <summary>
+        /// Coluna principal da resposta esperada possui item correspondente na coluna associada.
+        /// </summary>
+        public void AssineRegraItemColunaPrincipalPossuiColunaAssociada()
+        {
+            RuleFor(questao => questao.RespostaEsperada.Colunas)
+                        .Must(coluna => coluna != null && !coluna.Exists(item =>
+                                            (!string.IsNullOrEmpty(item.ColunaPrincipal.Letra) &&
+                                            string.IsNullOrEmpty(item.ColunaAssociada.Letra)) ||
+                                            (!string.IsNullOrEmpty(item.ColunaAssociada.Letra) &&
+                                            string.IsNullOrEmpty(item.ColunaPrincipal.Letra))
+                                        )
+                        )
+                        .WithMessage("A coluna principal deve possuir algum item na coluna associada.");
+        }
+
         protected override void AssineRegrasDeAtualizacao()
         {
             AssineRegraTipoQuestaAssociacaoDeColunas();
+            AssineRegraColunaPrincipalPossuiItem();
+            AssineRegraColunaAssociadaPossuiItem();
+            AssineRegraItemColunaPrincipalPossuiColunaAssociada();
+            AssineRegraDeveTerEnunciado();
+            AssineRegraAutorDaQuestaoInformadoEValido();
+            AssineRegraAutorDaQuestaoExiste();
+            AssineRegraAutorDaQuestaoNaoPodeSerAtualizado();
+            AssineRegraDificuldadeFoiInformadaEValida();
+            AssineRegraDisciplinaFoiInformada();
+            AssineRegraAreaDeConhecimentoFoiInformada();
         }
 
         protected override void AssineRegrasDeCadastro()
         {
             AssineRegraTipoQuestaAssociacaoDeColunas();
+            AssineRegraColunaPrincipalPossuiItem();
+            AssineRegraColunaAssociadaPossuiItem();
+            AssineRegraItemColunaPrincipalPossuiColunaAssociada();
+            AssineRegraDeveTerEnunciado();
+            AssineRegraAutorDaQuestaoInformadoEValido();
+            AssineRegraAutorDaQuestaoExiste();
+            AssineRegraAutorDaQuestaoNaoPodeSerAtualizado();
+            AssineRegraDificuldadeFoiInformadaEValida();
+            AssineRegraDisciplinaFoiInformada();
+            AssineRegraAreaDeConhecimentoFoiInformada();
         }
 
         protected override void AssineRegrasDeExclusao()
